8a8229b5a4faaa6c37264badbefef49f0db9f804
nanwar0 <nina.anwar@chicagobooth.edu>
2024-07-14 21:54:14 +0000

    routes and models created

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -2,4 +2,24 @@ class MiscController < ApplicationController
   def homepage
     render({ :template => "misc_templates/home"})
   end
+
+  def all_directors
+    render({ :template => "misc_templates/all_directors"})
+  end
+  
+  def youngest_director
+    render({ :template => "misc_templates/youngest_director"})
+  end
+
+  def eldest_director
+    render({ :template => "misc_templates/eldest_director"})
+  end
+
+  def all_movies
+    render({ :template => "misc_templates/all_movies"})
+  end
+
+  def all_actors
+    render({ :template => "misc_templates/all_actors"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+  get("/directors", { :controller => "misc", :action => "all_directors" })
+  get("/directors/youngest", { :controller => "misc", :action => "youngest_director" })
+  get("/directors/eldest", { :controller => "misc", :action => "eldest_director" })
+  get("/movies", { :controller => "misc", :action => "all_movies" })
+  get("/actors", { :controller => "misc", :action => "all_actors" })
 end
